a5d28cdc585100913fa621e069ebd7d9106b70c8
aiq-9 <alexkaufman78@gmail.com>
2024-10-19 15:18:52 -0400

    29. Intro to Events

--- src/main/java/net/aiq9/kaupencoursev2/KaupenCourseV2.java
+++ src/main/java/net/aiq9/kaupencoursev2/KaupenCourseV2.java
@@ -2,11 +2,13 @@ package net.aiq9.kaupencoursev2;
 
 import net.aiq9.kaupencoursev2.block.ModBlocks;
 import net.aiq9.kaupencoursev2.component.ModDataComponentTypes;
+import net.aiq9.kaupencoursev2.event.AttackEntityHandler;
 import net.aiq9.kaupencoursev2.item.ModItemGroups;
 import net.aiq9.kaupencoursev2.item.ModItems;
 import net.aiq9.kaupencoursev2.util.HammerUsageEvent;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import org.slf4j.Logger;
@@ -27,5 +29,7 @@ public class KaupenCourseV2 implements ModInitializer {
 		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
 
 		ModDataComponentTypes.registerDataComponentTypes();
+
+		AttackEntityCallback.EVENT.register(new AttackEntityHandler());
 	}
 }
